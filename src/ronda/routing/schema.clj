8b142c1f21d3f34b3f1ca29251b7ef914f4122d5
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-03-11 17:58:00 +0100

    allow for options.

--- src/ronda/routing/schema.clj
+++ src/ronda/routing/schema.clj
@@ -35,20 +35,22 @@
 
    This expects such a schema to be present, as opposed to `wrap-schemas`
    which will check for its existence."
-  [handler]
-  (fn [request]
-    (sm/handle-with-validation
-      handler
-      (attached-schema request)
-      request)))
+  [handler & [options]]
+  (let [options (or options {})]
+    (fn [request]
+      (sm/handle-with-validation
+        handler
+        (attached-schema request)
+        request
+        options))))
 
 (defn wrap-schemas
   "Wrap the given handler to validate requests against a schema that was
    attached to it by the `RouteDescriptor`-based routing middleware.
 
    If no schema is found, the request will only be normalized."
-  [handler]
-  (let [schema-handler (wrap-schemas* handler)
+  [handler & [options]]
+  (let [schema-handler (wrap-schemas* handler options)
         other-handler  (comp handler normalize-request)]
     (fn [request]
       (if (has-schema? request)
