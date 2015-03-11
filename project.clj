f9198546331a55d164e265f73f62985678381f84
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-03-11 18:03:52 +0100

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ronda/routing-schema "0.1.2-SNAPSHOT"
+(defproject ronda/routing-schema "0.1.2"
   :description "ronda RouteDescriptor-based schema validation."
   :url "https://github.com/xsc/ronda-routing-schema"
   :license {:name "Eclipse Public License"
