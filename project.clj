b9845c18a7d698f1d3c2322d1268fea54d175ed1
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-03-11 17:57:54 +0100

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [ronda/routing "0.1.0"]
-                 [ronda/schema "0.1.0-RC2"]]
+                 [ronda/routing "0.1.3"]
+                 [ronda/schema "0.1.0-RC3"]]
   :profiles {:dev {:plugins [[codox "0.8.10"]]
                    :codox {:project {:name "ronda/routing-schema"}
                            :src-dir-uri "https://github.com/xsc/ronda-routing-schema/blob/master/"
