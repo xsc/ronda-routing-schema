42576f6a78dea8bc9d7d1eec12aed2c52673738f
Marek Kubica <marek.kubica@stylefruits.de>
2015-08-17 14:02:07 +0200

    Bump dependencies for custom coercion factory

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "http://opensource.org/licenses/MIT"
             :year 2015
             :key "mit"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [ronda/routing "0.2.4"]
-                 [ronda/schema "0.1.0-RC3"]]
+                 [ronda/schema "0.1.3" :exclusions [potemkin]]]
   :profiles {:dev {:plugins [[codox "0.8.10"]]
                    :codox {:project {:name "ronda/routing-schema"}
                            :src-dir-uri "https://github.com/xsc/ronda-routing-schema/blob/master/"
